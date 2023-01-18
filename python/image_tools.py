716892033550fc09e10501f060e3f0e73ff07213
M4rkus- <M4rkus-@users.noreply.github.com>
2023-01-18 18:40:31 +0100

    Update image_tools.py

--- python/image_tools.py
+++ python/image_tools.py
@@ -39,3 +39,31 @@ if r.status_code == 200:
 else:
     print("Error pulling image")
 
+##################################
+
+import boto3
+
+# Create a client object
+client = boto3.client('ecr')
+
+# Define the registry, repository and image details
+registry_id = 'aws_account_id'
+repository_name = 'myimage'
+image_tag = 'latest'
+
+# Construct the full image name
+image_name = f"{registry_id}.dkr.ecr.us-east-1.amazonaws.com/{repository_name}:{image_tag}"
+
+# Pull the image
+response = client.batch_get_image(
+    registryId=registry_id,
+    repositoryName=repository_name,
+    imageIds=[
+        {
+            'imageTag': image_tag
+        },
+    ]
+)
+print(response)
+
+
