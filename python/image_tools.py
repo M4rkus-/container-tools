ed37edba8ef63eb910b6048a55a41177e5268edf
M4rkus- <M4rkus-@users.noreply.github.com>
2023-01-18 18:34:19 +0100

    Update image_tools.py

--- python/image_tools.py
+++ python/image_tools.py
@@ -16,3 +16,26 @@ if r.status_code == 200:
     print("Image pulled successfully")
 else:
     print("Error pulling image")
+
+#######################################
+
+import requests
+
+artifactory_url = "https://artifactory.example.com"
+image_name = "myimage:latest"
+repo = "docker-local"
+username = "myuser"
+password = "mypassword"
+
+# construct the url for pulling the image
+pull_url = f"{artifactory_url}/{repo}/{image_name}/latest"
+
+# send request to pull the image
+r = requests.get(pull_url, auth=(username, password))
+
+# check if the pull is successful
+if r.status_code == 200:
+    print("Image pulled successfully")
+else:
+    print("Error pulling image")
+
